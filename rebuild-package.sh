c1b16c6a95cbce2dee193d5884eae3142cbe6d0a
Pavlo Rudy <paulcarroty@users.noreply.github.com>
2022-06-02 23:38:46 +0300

    add max_parallel_downloads

--- rebuild-package.sh
+++ rebuild-package.sh
@@ -10,6 +10,7 @@ swupd update --quiet
 swupd bundle-add curl dnf git --quiet 
 git clone https://github.com/clearfraction/"$1".git && mv "$1"/* .
 # manage dependencies
+mkdir /etc/dnf && echo "max_parallel_downloads=20" >> /etc/dnf/dnf.conf
 shopt -s expand_aliases && alias dnf='dnf -q -y --releasever=latest --disableplugin=changelog,needs_restarting'
 dnf config-manager --add-repo https://cdn.download.clearlinux.org/current/x86_64/os
 dnf groupinstall build srpm-build && dnf install createrepo_c
